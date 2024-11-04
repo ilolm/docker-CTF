36a15c8d2faa804beb3bb37e338e7c5be4927875
Ilolm <kolya.ogorodnik.roma@gmail.com>
2024-11-04 19:40:41 +0200

    Added pre-installed images
    In order to reduce time consumed to deploy container. Unfortunately only php-fpm and nginx were added, mysql wasn't added because its huge size. In future I'm gonna try to bypass those issues related to constant time consumers

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,13 @@
 
 set -e
 
-dockerd &
-/usr/sbin/sshd -D
+# Starting docker daemon + starting ssh daemon
+dockerd > /dev/null 2>&1 &
+/usr/sbin/sshd -D > /dev/null 2>&1 &
+
+# Adding images
+docker image load -i /docker-images/nginx.tar
+docker image load -i /docker-images/php-fpm.tar
+
+# Cleaning images
+rm -rf /docker-images

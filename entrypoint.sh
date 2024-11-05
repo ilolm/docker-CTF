c4c3af0edc77ff9572affc95d581e705380fdb6f
Ilolm <kolya.ogorodnik.roma@gmail.com>
2024-11-05 16:24:08 +0200

    Update entrypoint.sh (see description)
    1. Added waiting for the docker engine to start.
    2. Added docker compose deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,14 @@ set -e
 dockerd > /dev/null 2>&1 &
 /usr/sbin/sshd -D > /dev/null 2>&1 &
 
+# Waiting for docker engine to start (until 'docker info' returns non-zero exit-code sleep 1)
+until docker info >/dev/null 2>&1; do sleep 1; done
+
 # Adding php image
 docker image load -i /docker-images/php-fpm.tar
 
 # Cleaning images
 rm -rf /docker-images
+
+# starting docker compose
+docker compose -f /home/king/docker-web/docker-compose.yaml up

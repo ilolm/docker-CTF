33262ae0716a45f51966db6ab0e1467f9d662fd3
Ilolm <kolya.ogorodnik.roma@gmail.com>
2024-11-05 16:24:08 +0200

    Update entrypoint.sh (see description)
    1. Added waiting for the docker engine to start.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,9 @@ set -e
 dockerd > /dev/null 2>&1 &
 /usr/sbin/sshd -D > /dev/null 2>&1 &
 
+# Waiting for docker engine to start (until 'docker info' returns non-zero exit-code sleep 1)
+until docker info >/dev/null 2>&1; do sleep 1; done
+
 # Adding php image
 docker image load -i /docker-images/php-fpm.tar
 
